f82f56e41bf81a8a40c235d288d0c82aae5c319c
Otávio <otaviocap@users.noreply.github.com>
2018-11-07 12:11:17 -0200

    Adding maps

---
+++ Maps/Dungeon3-16x16-itens.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Dungeon3-16x16-itens" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../Dungeon3-16x16-itens.png" width="256" height="256"/>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="100"/>
+   <frame tileid="153" duration="100"/>
+   <frame tileid="154" duration="100"/>
+   <frame tileid="155" duration="100"/>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="157" duration="100"/>
+   <frame tileid="158" duration="100"/>
+   <frame tileid="159" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="168">
+  <animation>
+   <frame tileid="168" duration="100"/>
+   <frame tileid="169" duration="100"/>
+   <frame tileid="170" duration="100"/>
+   <frame tileid="171" duration="100"/>
+   <frame tileid="172" duration="100"/>
+   <frame tileid="173" duration="100"/>
+   <frame tileid="174" duration="100"/>
+   <frame tileid="175" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Maps/Dungeon3-16x16.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Dungeon3-16x16" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../Dungeon3-16x16.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="Wall" tile="0"/>
+  <terrain name="Ground" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="16" terrain=",0,,0"/>
+ <tile id="18" terrain="0,,0,"/>
+ <tile id="35" terrain="0,,0,0"/>
+ <tile id="36" terrain=",0,0,0"/>
+ <tile id="49" terrain="0,0,0,0"/>
+ <tile id="97" terrain=",,,1"/>
+ <tile id="98" terrain=",,1,1"/>
+ <tile id="99" terrain=",,1,"/>
+ <tile id="113" terrain=",1,,1"/>
+ <tile id="114" terrain="1,1,1,1"/>
+ <tile id="115" terrain="1,,1,"/>
+</tileset>
