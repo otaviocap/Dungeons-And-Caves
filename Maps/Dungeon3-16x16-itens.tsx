c83cd9a57e71e98cbdc5117f792a662705dde2fa
Otávio <otaviocapo@gmail.com>
2018-11-08 11:41:21 -0200

    Update

--- Maps/Dungeon3-16x16-itens.tsx
+++ Maps/Dungeon3-16x16-itens.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="Dungeon3-16x16-itens" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../Dungeon3-16x16-itens.png" width="256" height="256"/>
+ <image source="../Assets/Dungeon3-16x16-itens.png" width="256" height="256"/>
  <tile id="152">
   <animation>
    <frame tileid="152" duration="100"/>
--- Maps/Dungeon3-16x16.tsx
+++ Maps/Dungeon3-16x16.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="Dungeon3-16x16" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../Dungeon3-16x16.png" width="256" height="256"/>
+ <image source="../Assets/Dungeon3-16x16.png" width="256" height="256"/>
  <terraintypes>
   <terrain name="Wall" tile="0"/>
   <terrain name="Ground" tile="0"/>
